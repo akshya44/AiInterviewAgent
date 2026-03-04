31a111b0a946ccdc5265c5fc0c9f9b62b9bcd479
Akshya Kumar sahoo <sakshyakumar44@gmail.com>
2026-03-05 01:05:32 +0530

    chore: remove all Firebase and MongoDB references, clean up test files

--- server/test.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-import('./index.js').catch(e => {
-    fs.writeFileSync('THE_ERROR.txt', e.stack || e.toString());
-});
--- server/testRequire.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const pdfParse = require("pdf-parse");
-console.log("KEYS:", Object.keys(pdfParse));
-if (typeof pdfParse === 'function') console.log("Is function");
-if (typeof pdfParse.default === 'function') console.log("Has default function");
