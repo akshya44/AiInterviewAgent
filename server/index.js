d70623bfd69f3c532a70b907b6a86d4521caa097
Akshya Kumar sahoo <sakshyakumar44@gmail.com>
2026-03-06 21:18:32 +0530

    fix express 5 wildcard path error on render

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'production' || process.env.RAILWAY_ENVIRONMENT) {
     const clientDistPath = path.join(__dirname, '../client/dist');
     app.use(express.static(clientDistPath));
 
-    app.get('*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(clientDistPath, 'index.html'));
     });
 } else {
